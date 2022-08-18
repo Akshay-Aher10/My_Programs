1fabd410f2171e4c24bee35c436b9af0e17115b9
Akshay Aher <103435669+Akshay-Aher10@users.noreply.github.com>
2022-08-18 10:12:07 +0530

    Add files via upload

---
+++ Assignment 16/Problem5.c
@@ -0,0 +1,58 @@
+// Accept number of rows and  number of column from user and print below pattern
+/*
+
+intput : iRow =4  and  iCol=4
+
+1       2       3       4
+1       *       *       4
+1       *       *       4
+1       2       3       4
+
+*/
+
+#include<stdio.h>
+
+void Display(int iRow,int iCol)
+{
+	int i=0,j=0;
+	
+	for(i=1;i<=iRow;i++)
+	{
+		for(j=1;j<=iCol;j++)
+		{			
+			if(i==1 || i==iRow)
+			{
+				printf("%d\t",j);
+			}
+			else if(j==1)
+			{
+				printf("1\t");
+			}
+			else if(j==iCol)
+			{
+				printf("%d\t",iCol);
+			}
+			else
+			{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int iValue1=0;
+	int iValue2=0;
+	
+	printf("Enter number of row\n");
+	scanf("%d",&iValue1);
+	
+	printf("Enter number of column\n");
+	scanf("%d",&iValue2);
+
+
+		Display(iValue1,iValue2);
+	
+	return 0;
+}
